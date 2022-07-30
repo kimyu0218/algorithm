77124539aba9646d6542973f095d1537f9177e8b
kimyu0218 <serena35@ewhain.net>
2022-07-30 22:30:53 +0900

    [그리디 알고리즘] 2022년 7월 30일

--- Baekjoon/그리디 알고리즘/13975.cpp
+++ Baekjoon/그리디 알고리즘/13975.cpp
@@ -3,31 +3,31 @@
 
 using namespace std;
 
-long long cost(int k, priority_queue<long long, vector<long long>, greater<>> chap) { // 최소 비용 구하기
+long long cost(int k, priority_queue<long long, vector<long long>, greater<>> file) { // 최소 비용 구하기
     long long sum = 0; // 최소 비용 저장하는 변수
-    while(!chap.empty()) {
-        if(chap.size() == 1) break;
-        long long tmp1 = chap.top(); chap.pop();
-        long long tmp2 = chap.top(); chap.pop();
+    while(!file.empty()) {
+        if(file.size() == 1) break;
+        long long tmp1 = file.top(); file.pop();
+        long long tmp2 = file.top(); file.pop();
         sum += (tmp1 + tmp2); // (tmp1 + tmp2 == 임시 파일)
-        chap.push(tmp1 + tmp2);
+        file.push(tmp1 + tmp2);
     }
     return sum;
 }
 
 int main() {
-    int t, k, file;
+    int t, k, f;
     cin >> t; // 테스트 개수 입력
 
     while(t--) {
         cin >> k; // 소설 장수 입력
 
-        priority_queue<long long, vector<long long>, greater<>> chapter; // (top이 가장 작은 수를 가리킴)
+        priority_queue<long long, vector<long long>, greater<>> file; // (top이 가장 작은 수를 가리킴)
         for(int i = 0; i < k; i++) {
-            cin >> file;
-            chapter.push(file);
+            cin >> f;
+            file.push(f);
         }
-        cout << cost(k, chapter) << '\n';
+        cout << cost(k, file) << '\n';
     }
     return 0;
 }
