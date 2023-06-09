14b3fafdcb8013eee4ce5c2fc660cb2d410ee180
kimyu0218 <serena35@ewhain.net>
2023-06-09 18:23:00 +0900

    [DFS & BFS] 2023년 06월 09일

---
+++ Baekjoon/DFS & BFS/13459.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+typedef pair<int, int> pi;
+const int CNT = 10;
+struct info {
+    pi red, blue;
+    int cnt;
+};
+int dr[4] = {0, 0, -1, 1}, dc[4] = {-1, 1, 0, 0}; // 좌 우 상 하
+
+void move(int &r, int &c, int dir, vector<vector<char>> &board) {
+    while(board[r][c] != 'O' && board[r][c] != '#') {
+        r += dr[dir];
+        c += dc[dir];
+    }
+    if(board[r][c] == '#') {
+        r -= dr[dir];
+        c -= dc[dir];
+    }
+}
+
+bool bfs(int n, int m, pi red, pi blue, vector<vector<char>> &board) {
+    vector<vector<vector<vector<bool>>>> visited (n, vector<vector<vector<bool>>> (m, vector<vector<bool>> (n, vector<bool> (m, false))));
+    queue<info> q;
+    q.push({red, blue, 0});
+
+    while(!q.empty()) {
+        red = q.front().red;
+        blue = q.front().blue;
+        int cnt = q.front().cnt;
+        q.pop();
+
+        if(cnt == CNT) { // 10번 이하로 움직여서 빼낼 수 없는 경우 stop
+            return false;
+        }
+
+        for(int i = 0; i < 4; i++) {
+            int rr = red.first, rc = red.second;
+            int br = blue.first, bc = blue.second;
+
+            move(rr, rc, i, board); // 빨간 구슬 이동
+            move(br, bc, i, board); // 파란 구슬 이동
+
+            if(board[br][bc] == 'O') { // 1-1. 파란 구슬 탈출 불가
+                continue;
+            }
+            if(board[rr][rc] == 'O') { // 1-2. 빨간 구슬 탈출 성공
+                return true;
+            }
+            if(rr == br && rc == bc) { // 1-3. 빨간 구슬과 파란 구슬 같은 칸에 있을 수 없음
+                int rd = abs(red.first - rr) + abs(red.second - rc);
+                int bd = abs(blue.first - br) + abs(blue.second - bc);
+                if(rd > bd) { // 빨간 구슬이 더 많이 움직인 경우
+                    rr -= dr[i];
+                    rc -= dc[i];
+                }
+                else { // 파란 구슬이 더 많이 움직인 경우
+                    br -= dr[i];
+                    bc -= dc[i];
+                }
+            }
+            if(visited[rr][rc][br][bc]) { // 이미 방문한 경우 pass
+                continue;
+            }
+            visited[rr][rc][br][bc] = true;
+            q.push({{rr, rc}, {br, bc}, cnt+1});
+        }
+    }
+    return false; // 구슬 탈출 불가
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n, m;
+    string input;
+    pi red, blue;
+    vector<vector<char>> board;
+
+    cin >> n >> m;
+    board.assign(n, vector<char> (m, '\0'));
+
+    for(int i = 0; i < n; i++) {
+        cin >> input;
+        for(int j = 0; j < m; j++) {
+            board[i][j] = input[j];
+            if(board[i][j] == 'R') {
+                red = {i, j};
+                board[i][j] = '.';
+            }
+            else if(board[i][j] == 'B') {
+                blue = {i, j};
+                board[i][j] = '.';
+            }
+        }
+    }
+
+    cout << bfs(n, m, red, blue, board);
+    return 0;
+}
---
+++ Baekjoon/DFS & BFS/13460_백트래킹ver.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+typedef pair<int, int> pi;
+const int CNT = 10;
+
+int answer = -1;
+int dr[4] = {0, 0, -1, 1}, dc[4] = {-1, 1, 0, 0}; // 좌 우 상 하
+
+bool isRedFirst(pi &red, pi &blue, int dir) { // 빨간 구슬이 먼저 움직이는가
+    if(dir == 0) { // L 왼쪽으로 기울이기
+        return red.second < blue.second;
+    }
+    if(dir == 1) { // R 오른쪽으로 기울이기
+        return red.second > blue.second;
+    }
+    if(dir == 2) { // U 위쪽으로 기울이기
+        return red.first < blue.first;
+    }
+    return red.first > blue.first; // D 아래쪽으로 기울이기
+}
+
+bool canEscape(char color, int dir, pi &bead, vector<vector<char>> &board) {
+    int sr = bead.first, sc = bead.second;
+    board[sr][sc] = '.'; // 현재 구슬 빈칸 처리
+
+    int r = sr + dr[dir], c = sc + dc[dir];
+    while(board[r][c] == '.') { // 빈 칸인 경우, 계속 굴러감
+        r += dr[dir];
+        c += dc[dir];
+    }
+
+    // 1. 구멍에 빠지는 경우
+    if(board[r][c] == 'O') {
+        return true;
+    }
+    // 2. 장애물을 마주친 경우
+    int er = r - dr[dir], ec = c - dc[dir];
+    board[er][ec] = color; // 구슬이 멈추는 위치
+    bead = {er, ec};
+    return false;
+}
+
+void backtracking(int cnt, pi red, pi blue, vector<vector<char>> board) {
+    if(cnt == CNT) { // 탈출 불가
+        return;
+    }
+
+    for(int i = 0; i < 4; i++) { // i : 방향
+        bool red_first = isRedFirst(red, blue, i);
+        vector<vector<char>> tmp_board = board;
+        pi tmp_red = red, tmp_blue = blue;
+        // 1. 빨간 구슬이 먼저 움직이는 경우
+        if(red_first) {
+            bool r_escape = canEscape('R', i, tmp_red, tmp_board);
+            bool b_escape = canEscape('B', i, tmp_blue, tmp_board);
+            if(r_escape && !b_escape) { // 1-1. 빨간 구슬 탈출 성공
+                if(answer == -1) {
+                    answer = cnt+1;
+                }
+                else if(answer > cnt+1){
+                    answer = cnt+1;
+                }
+                return;
+            }
+            if(!r_escape && !b_escape) { // 1-2. 빨간 구슬, 파란 구슬 새 위치로 이동
+                backtracking(cnt+1, tmp_red, tmp_blue, tmp_board);
+            }
+        }
+            // 2. 파란 구슬이 먼저 움직이는 경우
+        else {
+            bool b_escape = canEscape('B', i, tmp_blue, tmp_board);
+            bool r_escape = canEscape('R', i, tmp_red, tmp_board);
+            if(r_escape && !b_escape) { // 2-1. 빨간 구슬 탈출 성공
+                if(answer == -1) {
+                    answer = cnt+1;
+                }
+                else if(answer > cnt+1){
+                    answer = cnt+1;
+                }
+                return;
+            }
+            if(!r_escape && !b_escape) { // 2-2. 빨간 구슬, 파란 구슬 새 위치로 이동
+                backtracking(cnt+1, tmp_red, tmp_blue, tmp_board);
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n, m;
+    string input;
+    pi red, blue;
+    vector<vector<char>> board;
+
+    cin >> n >> m;
+    board.assign(n, vector<char> (m, '\0'));
+    for(int i = 0; i < n; i++) {
+        cin >> input;
+        for(int j = 0; j < m; j++) {
+            board[i][j] = input[j];
+            if(board[i][j] == 'R') {
+                red = {i, j};
+            }
+            else if(board[i][j] == 'B') {
+                blue = {i, j};
+            }
+        }
+    }
+
+    backtracking(0, red, blue, board);
+    cout << answer;
+    return 0;
+}
