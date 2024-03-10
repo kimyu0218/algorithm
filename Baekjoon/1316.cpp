518e816e1441901e454b3fb422b36dbe993743e8
kimyu0218 <serena35@ewhain.net>
2024-03-11 04:25:39 +0900

    refactor: [문자열] 2024년 03월 11일

--- Baekjoon/1316.cpp
+++ Baekjoon/1316.cpp
@@ -5,41 +5,42 @@ using namespace std;
 
 const int SIZE = 26;
 
-bool isGroupWord(string &word) {
-    vector<bool> visited(SIZE, false); // 방문 여부
+bool is_group_word(string word) {
+    vector<bool> find (SIZE, false);
+    find[word[0] - 'a'] = true;
 
-    visited[word[0]-'a'] = true; // 첫번째 문자 세팅
     for(int i = 1; i < word.length(); i++) {
-        if(!visited[word[i]-'a']) { // 새로운 문자가 등장한 경우
-            visited[word[i]-'a'] = true;
+        if(word[i] == word[i-1]) {
+            continue;
         }
-        else if(word[i] != word[i-1]) { // 그룹단어가 아닌 경우
+        int idx = word[i] - 'a';
+        if(find[idx]) {
             return false;
         }
+        find[idx] = true;
     }
     return true;
 }
 
-int cntGroupWords(int n, vector<string> &words) { // 그룹단어 개수 세기
-    int cnt = 0;
-    for(int i = 0; i < n; i++) {
-        cnt += isGroupWord(words[i]);
+int solution(vector<string> words) {
+    int result = 0;
+    for(int i = 0; i < words.size(); i++) {
+        result += is_group_word(words[i]);
     }
-    return cnt;
+    return result;
 }
 
 int main() {
-    int n; // 단어의 개수
-    vector<string> words; // 단어
+    int n;
+    vector<string> words;
 
-    // 입력
     cin >> n;
+
     words.assign(n, "");
     for(int i = 0; i < n; i++) {
         cin >> words[i];
     }
 
-    // 연산 & 출력
-    cout << cntGroupWords(n, words);
+    cout << solution(words);
     return 0;
 }
