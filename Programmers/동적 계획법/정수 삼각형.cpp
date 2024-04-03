77d036772f32819060614bffc3c0132e431d6547
kimyu0218 <serena35@ewhain.net>
2024-04-03 20:09:33 +0900

    refactor: [동적 계획법] 2024년 04월 03일

--- Programmers/동적 계획법/정수 삼각형.cpp
+++ Programmers/동적 계획법/정수 삼각형.cpp
@@ -6,17 +6,24 @@ using namespace std;
 int solution(vector<vector<int>> triangle) {
     int answer = 0;
 
-    int size = triangle.size();
-    int dp[size][size]; dp[0][0] = triangle[0][0]; // 정수 삼각형 합 저장하는 배열
+    int n = triangle.size();
+    vector<vector<int>> dp (n, vector<int> (0));
+    for(int i = 0; i < n; i++) {
+        dp[i].assign(i + 1, 0);
+    }
+    dp[0][0] = triangle[0][0];
 
-    for(int i = 1; i < size; i++) { // (i행)
-        dp[i][0] = dp[i-1][0] + triangle[i][0]; // (0번째 열은 우상향 대각선 값만 받아옴)
-        for(int j = 1; j < i; j++)
-            dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]; // 좌상향, 우상향 중 큰 값 가져옴
-        dp[i][i] = dp[i-1][i-1] + triangle[i][i]; // (i번째 열은 좌상향 대각선 값만 받아옴)
+    for(int i = 1; i < n; i++) {
+        dp[i][0] = dp[i - 1][0] + triangle[i][0];
+        dp[i][i] = dp[i - 1][i - 1] + triangle[i][i];
+
+        for(int j = 1; j < i; j++) {
+            dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
+        }
     }
 
-    for(int i = 0; i < size; i++)
-        answer = max(answer, dp[size-1][i]);
+    for(int i = 0; i < n; i++) {
+        answer = max(answer, dp[n - 1][i]);
+    }
     return answer;
 }
