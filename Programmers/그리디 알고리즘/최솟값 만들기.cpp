eb7cdd390a94a6073e2773538eb8a67fdd5dcc3d
kimyu0218 <serena35@ewhain.net>
2024-04-07 02:11:10 +0900

    [그리디 알고리즘] 2024년 04월 06일

---
+++ Programmers/그리디 알고리즘/최솟값 만들기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> A, vector<int> B) {
+    int answer = 0;
+    int n = A.size();
+
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end(), greater<>());
+
+    for(int i = 0; i < n; i++) {
+        answer += A[i] * B[i];
+    }
+    return answer;
+}
