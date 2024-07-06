7340c3540eeea2509c78a65d7336def40e64dc62
kimyu0218 <serena35@ewhain.net>
2024-07-06 23:56:49 +0900

    move: [정수론 및 조합론] 2024년 07월 06일

--- Programmers/정수론 및 조합론/멀쩡한 사각형.cpp
+++ Programmers/정수론 및 조합론/멀쩡한 사각형.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 typedef long long ll;
 
-int gcd(int a, int b) { // 최대 공약수 구하기
+int gcd(int a, int b) {
     while(b > 0) {
         int c = a % b;
         a = b;
@@ -14,10 +14,9 @@ int gcd(int a, int b) { // 최대 공약수 구하기
 }
 
 ll solution(int w,int h) {
-    if(w == h) { // 1. 가로 세로 길이가 같은 경우
+    if(w == h) {
         return (ll) w * h - w;
     }
-    // 2. 가로 세로 길이가 다른 경우
     int g = gcd(max(w, h), min(w, h));
     return (ll) w * h - (w/g + h/g - 1) * g;
 }
