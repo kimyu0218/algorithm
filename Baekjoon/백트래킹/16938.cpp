0a775d5453151b39739cd3e3865497293314b673
kimyu0218 <serena35@ewhain.net>
2024-10-05 23:48:47 +0900

    rename: [백트래킹] 2024년 10월 05일

--- Baekjoon/백트래킹/16938.cpp
+++ Baekjoon/백트래킹/16938.cpp
@@ -8,13 +8,14 @@ int answer = 0;
 int arr[SIZE];
 
 void backtracking(int idx, int cnt, int m, int n, int l, int r, int x, vector<int> &a) {
-    if(cnt == m) { // 문제 m개 고르기
+    if(cnt == m) {
         int min_num = MAX, max_num = 1, sum = 0;
         for(int i = 0; i < m; i++) {
             sum += arr[i];
             min_num = min(min_num, arr[i]);
             max_num = max(max_num, arr[i]);
         }
+
         if(sum >= l && sum <= r && max_num - min_num >= x) {
             answer++;
         }
@@ -40,6 +41,7 @@ int main() {
     for(int i = 2; i <= n; i++) {
         backtracking(0, 0, i, n, l, r, x, a);
     }
+    
     cout << answer;
     return 0;
 }
