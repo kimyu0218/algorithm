2b95de131ca5403d6ffb02b3eaf9afb0f0b2fbe0
kimyu0218 <serena35@naver.com>
2025-03-04 23:29:27 +0900

    Bruteforce

--- Baekjoon/3000-3999/3077.cpp
+++ Baekjoon/3000-3999/3077.cpp
@@ -3,34 +3,48 @@
 #include <map>
 
 using namespace std;
-vector<string> ans; // 정답
-map<string, int> test; // 현우가 적은 답안
 
-int score(int n) { // 점수 계산
+map<string, int> make_map(int n, vector<string> answer) {
+    map<string, int> m;
+    for(int i = 0; i < n; i++) {
+        m[answer[i]] = i + 1;
+    }
+    return m;
+}
+
+string solution(vector<string> answer, vector<string> hyunwoo) {
     int score = 0;
-    for(int i = 0; i < n-1; i++) {
-        for(int j = i+1; j < n; j++) { // (j는 항상 i보다 뒷순서의 전투를 가리킨다)
-            if(test[ans[i]] < test[ans[j]]) score++; // 상대적인 관계를 맞춘 경우
+    int n = answer.size();
+
+    map<string, int> m = make_map(n, answer);
+
+    for(int i = 0; i < n; i++) {
+        string s1 = hyunwoo[i];
+        for(int j = i; j < n; j++) {
+            string s2 = hyunwoo[j];
+            if(m[s1] < m[s2]) {
+                score++;
+            }
         }
     }
-    return score;
+    return to_string(score) + '/' + to_string(n * (n - 1) / 2);
 }
 
 int main() {
     int n;
+    vector<string> answer, hyunwoo;
+
     cin >> n;
 
-    string battle;
-    ans.assign(n, "");
+    answer.assign(n, "");
+    for(int i = 0; i < n; i++) {
+        cin >> answer[i];
+    }
+    hyunwoo.assign(n, "");
+    for(int i = 0; i < n; i++) {
+        cin >> hyunwoo[i];
+    }
 
-    for(int i = 0; i < n; i++) { // 정답 입력 (전투 순서대로 입력)
-        cin >> battle;
-        ans[i] = battle;
-    }
-    for(int i = 0; i < n; i++) { // 현우 답안 입력 (key: 전투명, value: 순서)
-        cin >> battle;
-        test[battle] = i;
-    }
-    cout << score(n) << '/' << n*(n-1)/2; // 점수 출력
+    cout << solution(answer, hyunwoo);
     return 0;
 }
