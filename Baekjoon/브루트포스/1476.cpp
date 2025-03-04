5f4113996db953179db7f1a9b6425ef4bc9666d2
김유정 <serena35@ewhain.net>
2025-03-04 22:58:58 +0900

    refactor: [브루트포스] 2025년 03월 04일

--- Baekjoon/브루트포스/1476.cpp
+++ Baekjoon/브루트포스/1476.cpp
@@ -6,28 +6,31 @@ const int E = 15;
 const int S = 28;
 const int M = 19;
 
-int compute_year(int num, int max) {
-    int mod = num % max;
-    return mod == 0 ? max : mod;
+int compute_year(int year, int max) {
+  int mod = year % max;
+  return mod ? mod : max;
 }
 
 int solution(int e, int s, int m) {
-    int result = 1;
-    while(true) {
-        if(compute_year(result, E) == e 
-        && compute_year(result, S) == s
-        && compute_year(result, M) == m) {
-            return result;
-        }
-        result++;
+  int answer = 1;
+  
+  while(true) {
+    int tmp_e = compute_year(answer, E);
+    int tmp_s = compute_year(answer, S);
+    int tmp_m = compute_year(answer, M);
+    
+    if (tmp_e == e && tmp_s == s && tmp_m == m) {
+      return answer;
     }
+    answer++;
+  }
 }
 
 int main() {
-    int e, s, m;
-    
-    cin >> e >> s >> m;
-
-    cout << solution(e, s, m);
-    return 0;
+  int e, s, m;
+  
+  cin >> e >> s >> m;
+  
+  cout << solution(e, s, m);
+  return 0;
 }
